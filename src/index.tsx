4ac1e666bc9f801d9274fb30841e50c95a06a05b
AydarF <aydar_fayzullin@yahoo.com>
2020-05-02 23:06:04 -0400

    First commit

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = (): JSX.Element => {
+  return <h1></h1>;
+};
+
+export default App;
+
+const root = document.getElementById("app-root");
+ReactDOM.render(<App />, root);
