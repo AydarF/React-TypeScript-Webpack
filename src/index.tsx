361241ea66329ca4ef4db24d3c4f020b93993812
AydarF <aydar_fayzullin@yahoo.com>
2020-05-03 21:48:03 -0400

    Features: Create add a book, complete the book, delete the book functionalities

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-const App = (): JSX.Element => {
-  return <h1>Hello React TypeScript</h1>;
-};
+type FormElement = React.FormEvent<HTMLFormElement>;
 
-export default App;
+interface IBook {
+  text: string;
+  complete: boolean;
+}
+
+export default function App(): JSX.Element {
+  const [value, setValue] = useState<string>("");
+  const [books, setBooks] = useState<IBook[]>([]);
+
+  const handleSubmit = (e: FormElement): void => {
+    e.preventDefault();
+
+    addBook(value);
+    setValue("");
+  };
+
+  const addBook = (text: string): void => {
+    const newBooks: IBook[] = [...books, { text: text, complete: false }];
+    setBooks(newBooks);
+  };
+
+  const completeBook = (index: number): void => {
+    const newBooks: IBook[] = [...books];
+    newBooks[index].complete = !newBooks[index].complete;
+    setBooks(newBooks);
+  };
+
+  const deleteBook = (index: number): void => {
+    const newBooks: IBook[] = [...books];
+    newBooks.filter((book) => {
+      if (book.complete) {
+        newBooks.splice(index, 1);
+      }
+    });
+    setBooks(newBooks);
+  };
+
+  return (
+    <>
+      <h1>Book List App</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          placeholder="Add book!"
+          required
+        />
+        <button type="submit">Add Book</button>
+      </form>
+
+      <section>
+        {books.map((book: IBook, index: number) => (
+          <ul key={index}>
+            <li style={{ textDecoration: book.complete ? "line-through" : "" }}>
+              {book.text}{" "}
+              <button type="button" onClick={() => completeBook(index)}>
+                {book.complete ? "Incomplete" : "Complete"}
+              </button>
+              <button type="button" onClick={() => deleteBook(index)}>
+                Delete
+              </button>
+            </li>
+          </ul>
+        ))}
+      </section>
+    </>
+  );
+}
 
 const root = document.getElementById("app-root");
 ReactDOM.render(<App />, root);
